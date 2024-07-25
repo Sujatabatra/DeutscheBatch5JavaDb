06f033b65898f7653f2f1a48941082b82b441e99
Sujatabatra <sujatabatra@hotmail.com>
2024-07-25 16:52:15 +0530

    Add files via upload

---
+++ Day4/GreetSpringXMLConfigurationProject/src/com/sujata/consumer/MainGreeting.java
@@ -0,0 +1,46 @@
+package com.sujata.consumer;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sujata.producer.GoodMorning;
+import com.sujata.producer.Greet;
+import com.sujata.producer.WishingEveryone;
+
+public class MainGreeting {
+
+	public static void main(String[] args) {
+		/*
+		 * When we are consuming any component using spring:
+		 * we need not to instantiate(create object) of that component,
+		 * rather Spring container will going to run the life cycle of the component
+		 * 
+		 * Spring Container : BeanFactory or ApplicationContext
+		 * ;
+		 * Inversion of Control : seperating the dependecy logic from the 
+		 * actual bussiness logic of an application
+		 */
+		
+		/*
+		 * Spring Container : 
+		 * Spring Container require configuration file , which tells Spring Container about the components for which
+		 * spring container is supposed to run the life cycle .
+		 * 
+		 * Configuration file : . xml, .java, also annotations
+		 */
+		
+		ApplicationContext springContainer=new ClassPathXmlApplicationContext("greet.xml");
+		
+		WishingEveryone wishEveryone1=(WishingEveryone)springContainer.getBean("wish");
+		
+		wishEveryone1.wish();
+		
+//		Greet g=new GoodMorning(); // depedency of good morning class is name
+//		
+//		WishingEveryone wishingEveryone=new WishingEveryone();
+//		wishingEveryone.wish();
+
+	}
+
+}
---
+++ Day4/GreetSpringXMLConfigurationProject/src/com/sujata/producer/WishingEveryone.java
@@ -0,0 +1,32 @@
+package com.sujata.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+
+public class WishingEveryone {
+
+	/*
+	 * when @Autowire is having multiple candidates to be autowired,
+	 * we can pick one with the @Qualifier
+	 * and in our case Greet interface have two implementation classes , i.e two candidates can be autowired
+	 * so we picked one with @Qualifier
+	 */
+	//dependency
+	
+	private Greet greet;
+	
+	
+
+	public WishingEveryone(Greet greet) {
+		super();
+		this.greet = greet;
+	}
+
+
+
+	public void wish() {
+		greet.wish();
+	}
+}
